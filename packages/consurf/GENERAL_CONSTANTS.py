2eb894a93741a881013b90da75ae3a2c79537b10
Adam Caprez <acaprez2@unl.edu>
2024-01-24 18:55:00 -0600

    Add DB config.

---
+++ packages/consurf/GENERAL_CONSTANTS.py
@@ -0,0 +1,44 @@
+
+
+
+
+
+CLUSTALW = "clustalw"
+CLUSTALW_2_0_10 = "clustalw2"
+MAFFT_LINSI_GUIDANCE = "mafft" # v7.222
+PRANK = "prank"
+MUSCLE = "muscle"
+
+RATE4SITE  =  "rate4site"
+RATE4SITE_ML = "rate4site"
+RATE4SITE_SLOW = "rate4site_doublerep"
+
+BLASTALL = "blastall"
+BLASTPGP = "blastpgp"
+CS_BLAST = "csblast"
+CS_BLAST_DATA = "/util/opt/anaconda/deployed-conda-envs/packages/consurf/envs/consurf-1.0.0/data"
+BLAST_PATH = "/util/opt/anaconda/deployed-conda-envs/packages/consurf/envs/consurf-1.0.0/bin"
+NR_NUC_DB_FASTA = "/bioseq/FASTA/nt/nt.fa"
+NR_NUC_DB = "/work/HCC/BCRF/BLAST/nt"
+
+BLAST_MAX_HOMOLOGUES_TO_DISPLAY = 500
+FRAGMENT_OVERLAP = 0.10
+FRAGMENT_MINIMUM_LENGTH = 0.60
+MINIMUM_FRAGMENTS_FOR_MSA = 5
+LOW_NUM_FRAGMENTS_FOR_MSA = 10
+MAXIMUM_MODIFIED_PERCENT = 0.15
+
+SWISSPROT_DB = "/work/HCC/BCRF/BLAST/swissprot"
+UNIPROT_DB = "/work/HCC/BCRF/uniprot/latest"
+UNIREF90_DB = "/work/HCC/BCRF/uniprot/latest/uniref/uniref90"
+CLEAN_UNIPROT_DB = "/util/opt/anaconda/deployed-conda-envs/packages/consurf/envs/consurf-1.0.0/data/clean_uniprot"
+NR_PROT_DB = "/work/HCC/BCRF/BLAST/nr"
+
+UNIREF90_DB_FASTA = "/work/HCC/BCRF/uniprot/latest/uniref/uniref90/uniref90.fasta"
+SWISSPROT_DB_FASTA = "/work/HCC/BCRF/app_specific/consurf/1.0.0/uniprot_sprot.fasta"
+CLEAN_UNIPROT_DB_FASTA = "/util/opt/anaconda/deployed-conda-envs/packages/consurf/envs/consurf-1.0.0/data/uniprot/clean_uniprot.fa"
+NR_PROT_DB_FASTA = "/util/opt/anaconda/deployed-conda-envs/packages/consurf/envs/consurf-1.0.0/data/nr/nr.fa"
+UNIPROT_DB_FASTA = "/work/HCC/BCRF/app_specific/consurf/1.0.0/uniprot_trembl.fa"
+
+CD_HIT_DIR = "/util/opt/anaconda/deployed-conda-envs/packages/consurf/envs/consurf-1.0.0"
+
